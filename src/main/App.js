c294a1fc02c45df7da50807b61785d19ce91be26
Natanael Leite <natansl@gmail.com>
2019-02-18 13:13:55 -0300

    Criação do componente sobre

--- src/main/App.js
+++ src/main/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './App.css';
+import Sobre from '../Sobre/Sobre.js';
 
 class App extends Component {
   render() {
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Sobre />
       </div>
     );
   }

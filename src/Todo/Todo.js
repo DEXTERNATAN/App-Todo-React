8e713806422e071ce96c1cf62f846420e3a31c6f
Natanael Leite <natansl@gmail.com>
2019-02-20 13:01:52 -0300

    Adição de funcionalidade cadastrar tarefas

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
-import FirebaseService from '../services/FirebaseService'
+// import FirebaseService from '../services/FirebaseService'
+import axios from "axios";
+
 import PageHeader from '../template/pageHeader'
 import TodoList from './todoList'
 import TodoForm from './todoForm'
+
+const URL = 'https://tasks-43f03.firebaseio.com/tarefa.json';
 
 class Todo extends Component {
 
@@ -16,11 +20,25 @@ class Todo extends Component {
 
       this.handleChange = this.handleChange.bind(this);
       this.handleAdd = this.handleAdd.bind(this);
+
+      axios.get(URL).then(data => console.log('Dados buscados: ', data))
+
     }
 
     handleAdd(e) {
+
       e.preventDefault();
-      console.log('Funcionou', this.state);
+      
+      const { descricao } = this.state;
+      
+      let objToSubmit = {
+        descricao
+      }
+
+      axios.post(URL, objToSubmit)
+        .then(data => console.log('Dados: ', data))
+        .catch(error => console.log(error))
+        
     }
 
     handleChange(e) {
--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -1,6 +1,7 @@
 import {firebaseDatabase} from '../utils/firebaseUtils'
 
 export default class FirebaseService {
+
     static getDataList = (nodePath, callback, size = 10) => {
 
         let query = firebaseDatabase.ref(nodePath)
@@ -16,6 +17,13 @@ export default class FirebaseService {
         });
 
         return query;
+    };
+
+    static pushData = (node, objToSubmit) => {
+        const ref = firebaseDatabase.ref(node).push();
+        const id = firebaseDatabase.ref(node).push().key;
+        ref.set(objToSubmit);
+        return id;
     };
 
 }

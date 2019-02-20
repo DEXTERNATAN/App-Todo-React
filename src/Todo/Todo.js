737814bb9bb5a6ef9fc79f646f489f411f42be05
Natanael Leite <natansl@gmail.com>
2019-02-20 09:29:53 -0300

    Adicionando a state ao campo descricao da tarefa

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -8,18 +8,32 @@ class Todo extends Component {
 
     constructor(props){
       super(props)
+
+      this.state = {
+        descricao: '',
+        list: []
+      }
+
       this.handleAdd = this.handleAdd.bind(this);
+      this.handleChange = this.handleChange.bind(this);
     }
 
     handleAdd() {
-      console.log('Add', this);
+      console.log('Add', this.state);
+    }
+
+    handleChange(e) {
+      this.setState({ ...this.state, descricao: e.target.value})
     }
 
     render() {
         return (
           <div className="container">            
             <PageHeader Titulo="Tarefas" descricao="cadastre suas tarefas aqui"/>
-            <TodoForm handleAdd={this.handleAdd}/>
+            <TodoForm 
+              descricao={this.state.descricao} 
+              handleAdd={this.handleAdd} 
+              handleChange={this.handleChange} />
             <TodoList />
           </div>  
         )
--- src/Todo/todoForm.js
+++ src/Todo/todoForm.js
@@ -6,7 +6,10 @@ export default props => (
     <form>
       <div className="form-row">
         <div className="form-group col-md-11">
-          <input type="text" className="form-control" id="descricao" placeholder="Adicione uma tarefa" />
+          <input type="text" className="form-control" id="descricao" descricao={props.descricao} placeholder="Adicione uma tarefa" 
+            onChange={props.handleChange}
+            onClick={props.handleAdd}
+          />
         </div>
         <div className="form-group col-md-1">
             <IconButton  style='primary' icon='plus' onClick={props.handleAdd} />
